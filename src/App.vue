5cfec00f2792ffc8acaa805e3441693e18240ae1
Oriana Sanabria <orianasanabria2@gmail.com>
2021-04-18 02:24:20 -0400

    added styles

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <template>
   <div id="app">
-    <label for="input">Nombre: </label>
-    <input v-model="nombre_pokemon" type="text" />
-    <button @click="getData">Buscar</button>
-    <h1 class="title">{{ getName }}</h1>
-    <img :src="getImage" alt="" />
-    <h2>Movimientos</h2>
-    <ul>
-      <li v-for="(pokeMove, i) in getMoves" :key="i">
-        {{ pokeMove.move.name }}
-      </li>
-    </ul>
-    <h2>Habilidades</h2>
-    <ul>
-      <li v-for="(pokeAbility, i) in getAbilities" :key="i">
-        {{ pokeAbility.ability.name }}
-      </li>
-    </ul>
+    <div class="container mt-5">
+      <div class="row align-items-center">
+        <div class="input-wrapper text-center col-12 col-md-6 col-lg-6">
+          <h1 class="title">{{ getName }}</h1>
+          <img class="images" width="200px" :src="getImageFront" alt="" />
+          <img class="images" width="200px" :src="getImageBack" alt="" />
+          <div class="row">
+            <input
+              class="form-control col-4 me-2"
+              style="width: 300px"
+              v-model="nombre_pokemon"
+              type="text"
+            />
+            <button class="btn btn-danger col-3" @click="getData">
+              Search
+            </button>
+          </div>
+        </div>
+        <div class="info-wrapper col-12 col-md-6 col-lg-6 mt-4">
+          <ul class="row mb-5">
+            <h2 class="mb-4">Abilities</h2>
+            <li
+              class="items col-3"
+              v-for="(pokeAbility, i) in getAbilities"
+              :key="i"
+            >
+              {{ pokeAbility.ability.name }}
+            </li>
+          </ul>
+          <ul class="row moves-wrapper">
+            <h2 class="mb-4">Moves</h2>
+            <li
+              class="items col-3 mt-2"
+              v-for="(pokeMove, i) in getMoves"
+              :key="i"
+            >
+              {{ pokeMove.move.name }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,9 +62,7 @@ export default {
       try {
         const req = await axios(url + this.nombre_pokemon);
         if (!req) return;
-        console.log(req.data);
         this.pokeData = req.data;
-        console.log(this.pokeData);
       } catch (error) {
         console.log(error);
       }
@@ -52,11 +75,18 @@ export default {
     getName() {
       return this.pokeData.name;
     },
-    getImage() {
+    getImageFront() {
       return (
         this.pokeData &&
         this.pokeData.sprites &&
         this.pokeData.sprites.front_default
+      );
+    },
+    getImageBack() {
+      return (
+        this.pokeData &&
+        this.pokeData.sprites &&
+        this.pokeData.sprites.back_default
       );
     },
     getMoves() {
@@ -72,10 +102,25 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+}
+
+h1,
+h2 {
+  font-weight: bold;
+}
+
+ul > li {
+  list-style: none;
+  font-size: 0.7rem;
+}
+
+.moves-wrapper {
+  height: 290px;
+  overflow-y: hidden;
 }
 </style>
